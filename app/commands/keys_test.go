3d6a14c4f7ff3c115899b992377e8f5f75576f4a
Rafael Carvalho <az.carta@gmail.com>
2025-07-17 15:42:21 -0300

    feat: add unit tests for handleKeys function to validate KEYS command behavior and argument handling

---
+++ app/commands/keys_test.go
@@ -0,0 +1,63 @@
+package commands
+
+import (
+	"mini-redis-go/app/server_config"
+	"testing"
+)
+
+func Test_handleKeys(t *testing.T) {
+	type args struct {
+		args   []string
+		store  *map[string]Entry
+		config server_config.ServerConfig
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "returns error when not enough arguments are provided",
+			args: args{
+				args:   []string{"KEYS"},
+				store:  &map[string]Entry{},
+				config: server_config.ServerConfig{},
+			},
+			want: "-ERR wrong number of arguments for 'keys' command\r\n",
+		},
+		{
+			name: "returns unknown command error when pattern is not *",
+			args: args{
+				args:   []string{"KEYS", "foo"},
+				store:  &map[string]Entry{"foo": {Value: "bar", ExpiryTime: 0}},
+				config: server_config.ServerConfig{},
+			},
+			want: "-ERR unknown command foo\r\n",
+		},
+		{
+			name: "returns empty array when store is empty and pattern is *",
+			args: args{
+				args:   []string{"KEYS", "*"},
+				store:  &map[string]Entry{},
+				config: server_config.ServerConfig{},
+			},
+			want: "*0\r\n",
+		},
+		{
+			name: "returns all keys in RESP2 array when pattern is *",
+			args: args{
+				args:   []string{"KEYS", "*"},
+				store:  &map[string]Entry{"foo": {Value: "bar", ExpiryTime: 0}, "baz": {Value: "qux", ExpiryTime: 0}},
+				config: server_config.ServerConfig{},
+			},
+			want: "*2\r\n$3\r\nfoo\r\n$3\r\nbaz\r\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := handleKeys(tt.args.args, tt.args.store, tt.args.config); got != tt.want {
+				t.Errorf("handleKeys() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
